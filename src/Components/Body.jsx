94436d6309596479c6ccf4f5d6bacbe5e7a3192e
TejasBishnoi21 <tejas.vishnoi@gmail.com>
2022-12-15 21:16:02 +0530

    landing page_day3_commit-1

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,52 @@
+import React from "react";
+import { render } from "react-dom";
+import Carousel from "./Carousel";
+import '../Styles/body.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Body = ()=>{
-    return <div style={{'width':'90%', 'height':'500px', 'margin':'auto', 'border':'1px solid blue'}}>
-        <h1>This is Dummy Body </h1>
+
+    return <div style={{'width':'99%','padding':'5px', 'margin':'auto'}}>
+        <Carousel />
+
+        <div className="hot_deals">
+            <div><h3>HOT DEALS</h3></div>
+            <div className="hot_cards">
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F2db90f13-ccff-4706-90b3-85b024aa6020.jpg&w=1920&q=75" alt="1" width={'32%'}/>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F90a0f8b4-0ae4-4f0c-853d-ca54522fb509.jpg&w=1920&q=75" alt="2" width={'32%'}/>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Fd9560110-40f7-4d14-a98d-5599b45a84ef.jpg&w=1920&q=75" alt="3" width={'32%'}/>
+            </div>
+        </div>
         
+        <div className="sugar_hai">
+            <h3>SHUKAR HAI, SUGAR HAI</h3>
+            <div>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https://d32baadbbpueqt.cloudfront.net/Homepage/b16a42b1-3e35-42bb-b122-a517e75c490e.jpg&w=1920&q=75" alt="shukar hai" />
+            </div>
+        </div>
+
+        <div className="top_picks">
+            <div><h3>TOP PICKS OF THE WEEK</h3></div>
+            <div className="hot_cards">
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F4849cf44-c55c-4224-ba06-9dd0a5f48b67.gif&w=1920&q=75" alt="1" width={'32%'}/>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Fc5dc6c75-a49e-4028-b3a3-3bbbc63e49d6.jpg&w=1920&q=75" alt="2" width={'32%'}/>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F9f9d2095-f019-4bb5-b961-db87fab439f6.jpg&w=1920&q=75" alt="3" width={'32%'}/>
+            </div>
+        </div>
+
+        <div className="sugar_hai">
+            <h3>REFER YOUR FRIENDS</h3>
+            <div>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F8bfe8bb4-002d-4097-a773-6af33325a594.jpg&w=1920&q=75" alt="shukar hai" width={'100%'}/>
+            </div>
+        </div>
+
+        <div className="sugar_hai">
+            <h3>NEWLY LAUNCHED</h3>
+            <div>
+                <img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Ff190c764-84cf-4eea-a381-8b701be65852.gif&w=1920&q=75" alt="shukar hai" width={'100%'}/>
+            </div>
+        </div>
     </div>
 }
 
